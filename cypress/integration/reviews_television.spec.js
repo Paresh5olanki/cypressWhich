3c0d9cd91c774bffc51af1e0902a56f9f3253479
Paresh <paresh10980@gmail.com>
2021-05-18 10:04:14 +0100

    initial commit

---
+++ cypress/integration/reviews_television.spec.js
@@ -0,0 +1,14 @@
+const reviews = require('../support/pageData/reviews')
+
+describe('Review Televisions', () => {
+  beforeEach(() => {
+    cy.visit('/televisions')
+  })
+
+//   it('Should see 10 search results', () => {
+//   })
+//   it('Should see blurred elements for logged out user', () => {
+//   })
+//   it('Should see the correct result count for specif filters', () => {
+//   })
+})
